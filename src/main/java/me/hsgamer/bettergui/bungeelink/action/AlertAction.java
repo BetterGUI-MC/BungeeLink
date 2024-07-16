b73da2f094a96cb9c3d290ebadcc68dff8c15d26
hsgamer <huynhqtienvtag@gmail.com>
2024-07-16 11:02:49 +0700

    update plugin

--- src/main/java/me/hsgamer/bettergui/bungeelink/action/AlertAction.java
+++ src/main/java/me/hsgamer/bettergui/bungeelink/action/AlertAction.java
@@ -1,9 +1,10 @@
 package me.hsgamer.bettergui.bungeelink.action;
 
-import me.hsgamer.bettergui.api.action.BaseAction;
 import me.hsgamer.bettergui.builder.ActionBuilder;
+import me.hsgamer.bettergui.util.SchedulerUtil;
+import me.hsgamer.hscore.action.common.Action;
 import me.hsgamer.hscore.bukkit.channel.BungeeUtils;
-import me.hsgamer.hscore.bukkit.scheduler.Scheduler;
+import me.hsgamer.hscore.common.StringReplacer;
 import me.hsgamer.hscore.task.element.TaskProcess;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -11,22 +12,30 @@ import org.bukkit.plugin.Plugin;
 
 import java.util.UUID;
 
-public class AlertAction extends BaseAction {
+public class AlertAction implements Action {
     private final Plugin plugin;
+    private final String value;
 
     public AlertAction(Plugin plugin, ActionBuilder.Input input) {
-        super(input);
         this.plugin = plugin;
+        this.value = input.getValue();
     }
 
     @Override
-    public void accept(UUID uuid, TaskProcess process) {
+    public void apply(UUID uuid, TaskProcess process, StringReplacer stringReplacer) {
         Player player = Bukkit.getPlayer(uuid);
         if (player == null) {
             process.next();
             return;
         }
-        String replaced = getReplacedString(uuid);
-        Scheduler.current().sync().runEntityTaskWithFinalizer(player, () -> BungeeUtils.sendMessage(plugin, player, "ALL", replaced), process::next);
+        String replaced = stringReplacer.replaceOrOriginal(value, uuid);
+        SchedulerUtil.entity(player)
+                .run(() -> {
+                    try {
+                        BungeeUtils.sendMessage(plugin, player, "ALL", replaced);
+                    } finally {
+                        process.next();
+                    }
+                }, process::next);
     }
 }
